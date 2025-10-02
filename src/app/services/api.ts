3e36ebebf5a3cb86e7955caa7bf9b7e70360670d
unknown <evertonjose421@gmail.com>
2025-10-02 13:40:22 -0300

    021020251340

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -4,7 +4,9 @@ export function configApi (){
 
 const api = axios.create({
     //baseURL:"http://26.236.249.244:3333",
-    baseURL:"http://localhost:3333"
+    //baseURL:"http://localhost:3333"
+     baseURL:"https://192.168.100.106:3333"
+
 })
 
 
