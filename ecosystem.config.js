543e448f0b3a97a10561b0f598f369d7e1d9ffad
unknown <evertonjose421@gmail.com>
2025-10-24 17:02:21 -0300

    241020251702

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,15 +2,17 @@ module.exports = {
     apps:[
         {
             name: 'app-backups',
-            script: 'npm',
+            script: 'next start -p 8000',
+            interpreter: "node",
             args: 'start',
             cwd:'.',
             autorestart: true,
             watch:false,
-            max_memory_restart:'2G',
+            max_memory_restart:'1G',
             env:{
                 NODE_ENV: 'production',
-                PORT:8000 
+                PORT:8000,
+                NODE_TLS_REJECT_UNAUTHORIZED:'0'
             }
         }
     ]
