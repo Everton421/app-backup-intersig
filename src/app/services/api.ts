0bab8bc198a47edd9cc6c6a13b02c794cd4ea05e
unknown <evertonjose421@gmail.com>
2025-10-27 09:56:39 -0300

    271020250956

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export function configApi (){ 
-    let url ='https://localhost:3333'
+    let url ='https://192.168.100.106:3333'
 
     if(process.env.NEXT_PUBLIC_URL_API && process.env.NEXT_PUBLIC_URL_API != ''){
             url = process.env.NEXT_PUBLIC_URL_API
